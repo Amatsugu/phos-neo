55e9b5d8457f4cdff43f79f7c8679ee304ba340f
Amatsugu <khamraj@kaisei.app>
2024-11-15 21:56:55 -0500

    building

--- game/buildings/src/buildings/mod.rs
+++ game/buildings/src/buildings/mod.rs
@@ -1,5 +1,5 @@
-pub mod resource_gathering;
 pub mod basic_building;
-pub mod factory_building;
-pub mod tech_building;
 pub mod conduit_building;
+pub mod factory_building;
+pub mod resource_gathering;
+pub mod tech_building;
